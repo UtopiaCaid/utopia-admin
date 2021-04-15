a792059827671d3c61508fd29f3fc061fd4c256f
draggling <danielpwoo@gmail.com>
2021-04-15 16:14:10 -0400

    added cftemplate

--- aws-cf-deploy.sh
+++ aws-cf-deploy.sh
@@ -1,6 +1,6 @@
 
 aws cloudformation deploy --region us-east-2 --stack-name UtopiaAdminMS \
---template-file test-utopia-cftemplate --parameter-overrides ApplicationName=UtopiaAdminMS \
+--template-file test-adminutopia-cftemplate --parameter-overrides ApplicationName=UtopiaAdminMS \
 ECRepositoryUri=$AWS_ID/utopia-admin:$COMMIT_HASH DBUrl=$DB_URL DBUsername=$DB_USERNAME \
 DBPassword=$DB_PASSWORD ECSCluster=$UTOPIA_CLUSTER ExecutionRoleArn=$EXECUTION_ROLE_ARN \
 SubnetID=$UTOPIA_PRIVATE_SUBNET_1 TargetGroupArnDev=$TARGETGROUP_UTOPIA_ADMIN_DEV_ARN \
